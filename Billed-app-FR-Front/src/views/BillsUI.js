22b4eed1431d0893a878a93c4f88805b0a22e092
git504 <git504@mailo.com>
2022-03-01 10:08:36 +0100

    bug date

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -19,8 +19,16 @@ const row = (bill) => {
     `;
 };
 
+// architecture MVC model view controllers BUG.1
 const rows = (data) => {
-  return data && data.length ? data.map((bill) => row(bill)).join("") : "";
+  const sortedDataByDate =
+    data && data.length > 0
+      ? data.sort((a, b) => new Date(b.date) - new Date(a.date))
+      : "";
+
+  return data && data.length
+    ? sortedDataByDate.map((bill) => row(bill)).join("")
+    : "";
 };
 
 export default ({ data: bills, loading, error }) => {
